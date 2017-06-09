6b829b6cfdba4b8a542dc74c4cc91d9bc9c24e18
金伟强 <iamjoel007@gmail.com>
2017-06-09 10:02:00 +0800

    add svg

---
+++ svg/README.md
@@ -0,0 +1,7 @@
+# SVG 简历
+* 描线动画
+* 文字动画
+
+## 工具及参考链接
+* [Snap.svg](http://snapsvg.io/)
+* [SVG动画实践](http://weixin.github.io/resources/slide/cssconf2016/nikki/html/slides.html)
---
+++ svg/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('#main').fullpage({
+     sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+     css3: true,
+     scrollingSpeed: 1000
+  })
+})
